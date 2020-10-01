249d3495712689e8f8b845cb7a8eb494b7f8bc98
Adrian Pang <amsdkgh@students.cs.ubc.ca>
2020-10-01 12:50:50 -0700

    added test case for dataLoader

--- code/evaluation/dataLoader.py
+++ code/evaluation/dataLoader.py
@@ -1,13 +1,29 @@
 import csv, urllib.request
+import filecmp
+
+WRITE_OUTPUT_TO_FILE = True # change to True to look at .txt output
 
 def load_data(url: str):
     response = urllib.request.urlopen(url)
     lines = [l.decode('utf-8') for l in response.readlines()]
     data = csv.reader(lines)
 
+    if (WRITE_OUTPUT_TO_FILE):
+        text_file = open("dataLoaderOutputActual.txt", "w")
+
     for row in data:
         print(row)
+        if (WRITE_OUTPUT_TO_FILE):
+            text_file.write("[")
+            for s in row:
+                text_file.write("'"+s+"', ")
+            text_file.write("]\n")
     
+    if (WRITE_OUTPUT_TO_FILE):
+        text_file.close()
+        print("Assert data loaded as expected: ")
+        print(filecmp.cmp('dataLoaderOutputExpected.txt', 'dataLoaderOutputActual.txt'))
+
     return data
 
 load_data("http://winterolympicsmedals.com/medals.csv")
