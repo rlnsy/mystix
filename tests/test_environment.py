8350025f3f39dbbf7976c9f21fd85bc0bf86c0cb
UBC Student <rowdl22@students.cs.ubc.ca>
2020-10-09 14:31:29 -0700

    Add variable engine (#4)
    * implement memory and environment

---
+++ tests/test_environment.py
@@ -0,0 +1,71 @@
+import unittest
+from typing import cast
+
+from code.language.evaluation.vars import Environment, UndefinedVariableError
+from code.language.shared.primitives.values import IntegerValue, Value
+
+
+class EnvironmentTests(unittest.TestCase):
+
+    def test_undefined(self):
+        e = Environment()
+        try:
+            e.get_val("color")
+            self.fail()
+        except UndefinedVariableError:
+            pass
+
+    def test_undefined_undef(self):
+        e = Environment()
+        try:
+            e.undef("color")
+            self.fail()
+        except UndefinedVariableError:
+            pass
+
+    def test_set_no_extend(self):
+        e = Environment()
+        try:
+            e.set_val("width", IntegerValue(5))
+            self.fail()
+        except UndefinedVariableError:
+            pass
+
+    def test_basic(self):
+        e = Environment()
+        e.extend("width", IntegerValue(3))
+        e.extend("height", IntegerValue(4))
+        self.assertEqual(3, cast(IntegerValue, e.get_val("width")).value)
+        self.assertEqual(4, cast(IntegerValue, e.get_val("height")).value)
+
+    def test_basic_set(self):
+        e = Environment()
+        e.extend("width", IntegerValue(3))
+        self.assertEqual(3, cast(IntegerValue, e.get_val("width")).value)
+        e.set_val("width", IntegerValue(8))
+        self.assertEqual(8, cast(IntegerValue, e.get_val("width")).value)
+
+    def test_undef(self):
+        e = Environment()
+        e.extend("width", IntegerValue(3))
+        self.assertEqual(3, cast(IntegerValue, e.get_val("width")).value)
+        e.undef("width")
+        try:
+            e.get_val("width")
+            self.fail()
+        except UndefinedVariableError:
+            pass
+
+    def test_multiple_extend(self):
+        e = Environment()
+        e.extend("width", IntegerValue(3))
+        e.extend("width", IntegerValue(6))
+        self.assertEqual(6, cast(IntegerValue, e.get_val("width")).value)
+
+    def test_multiple_extend_undef(self):
+        e = Environment()
+        e.extend("width", IntegerValue(3))
+        e.extend("width", IntegerValue(6))
+        self.assertEqual(6, cast(IntegerValue, e.get_val("width")).value)
+        e.undef("width")
+        self.assertEqual(3, cast(IntegerValue, e.get_val("width")).value)
---
+++ tests/test_memory.py
@@ -0,0 +1,65 @@
+import unittest
+
+from code.language.evaluation.vars import Memory, OutOfMemoryError
+from code.language.shared.primitives.values import IntegerValue, Value
+
+
+class MemoryTests(unittest.TestCase):
+
+    def test_fresh_location_no_set(self):
+        m = Memory()
+        l1 = m.get_fresh_loc()
+        l2 = m.get_fresh_loc()
+        self.assertEqual(l1,l2)
+
+    def test_fresh_location_post_set(self):
+        m = Memory()
+        l1 = m.get_fresh_loc()
+        m.write(l1, Value())
+        l2 = m.get_fresh_loc()
+        self.assertNotEqual(l1, l2)
+
+    def test_set_get(self):
+        m = Memory()
+        l1 = m.get_fresh_loc()
+        m.write(l1, IntegerValue(22))
+        v: Value = m.read(l1)
+        self.assertTrue(isinstance(v, IntegerValue))
+        self.assertEqual(v.value, 22)
+
+    def test_free_location_post_set(self):
+        m = Memory()
+        l1 = m.get_fresh_loc()
+        m.write(l1, Value())
+        m.free(l1)
+        l2 = m.get_fresh_loc()
+        self.assertEqual(l1, l2)
+
+    def test_overwrite(self):
+        m = Memory()
+        l1 = m.get_fresh_loc()
+        m.write(l1, IntegerValue(22))
+        m.write(l1, IntegerValue(23))
+        v: Value = m.read(l1)
+        self.assertTrue(isinstance(v, IntegerValue))
+        self.assertEqual(v.value, 23)
+
+    def test_no_limit_on_fresh_no_write(self):
+        m = Memory()
+        for i in range(Memory.MAX_VALUES):
+            m.get_fresh_loc()
+        try:
+            m.get_fresh_loc()
+        except OutOfMemoryError:
+            self.fail()
+
+    def test_memory_limit(self):
+        m = Memory()
+        for i in range(Memory.MAX_VALUES):
+            l = m.get_fresh_loc()
+            m.write(l, IntegerValue(1))
+        try:
+            m.get_fresh_loc()
+            self.fail()
+        except OutOfMemoryError:
+            pass
