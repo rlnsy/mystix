67863dccb530fcbc962c401b1aa9709548410896
amsdkgh <adriansnpang@hotmail.com>
2020-10-12 15:17:34 -0700

    base case test for tokenizer, WIP

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,7 +1,5 @@
 import unittest as ut
 from typing import List
-from tests.util.example_ast import example_1
-from tests.util import ast_equal
 from code.ui.util import read_program_file
 from code.language.tokenization import tokenize
 from code.language.shared.ast import Program
@@ -9,7 +7,7 @@ from code.language.shared.ast import Program
 class TestTokenizer(ut.TestCase):
 
     def test_tokenizer_1(self):
-        def run_compile(content: List[str]) -> Program:
+        def run_compile(content: str) -> Program:
             return tokenize(content)
         p: Program = read_program_file("tests/res/programs/example1", run_compile)
-        self.assertTrue(ast_equal(p, example_1()))
+        self.assertTrue(p, str)
