833fa3d7b37fd1cbb79b60a7285b6392728e0ed2
sofiamc9 <sofiamc9@students.cs.ubc.ca>
2020-10-13 18:58:40 -0700

    calculations added to SimpleFunc

--- code/language/shared/ast/math_ast/SimpleFunc.py
+++ code/language/shared/ast/math_ast/SimpleFunc.py
@@ -10,3 +10,39 @@ class SimpleFunc(Func):
         self.impacted_var: Var = impacted_var
         self.op: Operand = op
         self.rhs = v
+        pass
+
+    operations = dict({
+        "+": "__calcSum",
+        "-": "__calcDif",
+        "*": "__calcMul",
+        "/": "__calcDiv",
+        "^": "__calcPwr"
+    })
+
+    def __calcSum(self):
+        self.impacted_var = self.impacted_var + self.rhs
+        pass
+
+    def __calcDif(self):
+        self.impacted_var = self.impacted_var - self.rhs
+        pass
+
+    def __calcMul(self, parameter_list): 
+        self.impacted_var = self.impacted_var * self.rhs
+        pass
+
+    def __calcDiv(self, parameter_list):
+        self.impacted_var = self.impacted_var / self.rhs
+        pass
+
+    def __calcPwr(self, parameter_list):
+        self.impacted_var = self.impacted_var ^ self.rhs
+        pass
+
+    def calculate(self):
+        # add error checking is op is invalid?
+        self.operations.get(self.op)()
+        pass
+
+    pass
