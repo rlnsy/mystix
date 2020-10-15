01140571e94abb1a1e25880eb6231f668e82c069
sofiamc9 <sofiamc9@students.cs.ubc.ca>
2020-10-15 01:09:06 -0700

    evaluator simpleFund and BuiltinFunc implemented

--- code/language/evaluation/evaluator.py
+++ code/language/evaluation/evaluator.py
@@ -3,9 +3,26 @@ from typing import TYPE_CHECKING as STATIC_CHECK
 if STATIC_CHECK:
     from code.language.shared.ast import *
 from code.language.shared.ast.visitor import Visitor
+from code.language.shared.primitives import values, numerical
+from code.targets.analysis.math import apply_fn, apply_op
+from .errors import LanguageError
+from .vars import Environment
 
 
 class Evaluator(Visitor):
+    # variable_table = {}
+    # data = {}
+
+    def __init__(self):
+        self.env = Environment()
+
+    # __operations = {
+    #     "+": "__Sum",
+    #     "-": "__Dif",
+    #     "*": "__Mul",
+    #     "/": "__Div",
+    #     "^": "__Pow"
+    # }
 
     def evaluate(self, p: Program):
         return self.visit_program(p)
@@ -29,7 +46,13 @@ class Evaluator(Visitor):
         pass
 
     def visit_trigger(self, t: Trigger):
-        pass
+        try:
+            data_source = self.data[t.var]
+            # implement Observer Pattern to watch for data updates
+        except:
+            raise KeyError("Cannot run mathematics on invalid data")
+        else:
+            t.math_funcs.accept()
 
     def visit_plotter(self, pltr: Plotter):
         pass
@@ -59,9 +82,29 @@ class Evaluator(Visitor):
         pass
 
     def visit_math_funcs(self, mf: MathFuncs):
+        for func in mf.mth_func_lst:
+            func.accept(self)
+            pass
         pass
 
-    def visit_simple_func(self, smp: SimpleFunc):
+    def visit_simple_func(self, smp: SimpleFunc) -> values.Value:
+
+        # if smp.impacted_var not in self.variable_table:
+        #     self.variable_table[smp.impacted_var.name] = 0
+        
+        # curr_val = self.variable_table[smp.impacted_var.name]
+        # num = 0
+        if isinstance(smp.rhs.value, values.NumericalValue):
+            v: values.Value = self.env.get_val(smp.impacted_var.name)
+            if isinstance(v, values.NumericalValue):
+                operand = smp.op.accept(self)
+                return apply_op(operand,v, smp.rhs.value)
+            else:
+                raise LanguageError("Simple Functions only accept numbers.")
+        else:
+            raise LanguageError("Simple Functions only accept numbers.")
+
+        # self.variable_table[smp.impacted_var.name] = self.__calculate(curr_val, smp.op, num)
         pass
 
     def visit_increment(self, i: Increment):
@@ -71,7 +114,38 @@ class Evaluator(Visitor):
         pass
 
     def visit_builtin_func(self, bltn: BuiltinFunc):
-        pass
+        v: values.Value = self.env.get_val(bltn.input.name)
+        if isinstance(v, values.NumericalValue):
+            return apply_fn(bltn.fun, v)
+        else:
+            raise LanguageError("BuiltIn Functions only accept numbers.")
 
-    def visit_operand(self, op: Operand):
-        pass
+    def visit_operand(self, op: Operand) -> numerical.NumOp:
+        return op.op
+
+    # """
+    # To call calculation methods dynamically using a dictionary.
+    # Per this post:
+    # https://stackoverflow.com/questions/16642145/how-to-dynamically-call-methods-within-a-class-using-method-name-assignment-to-a?lq=1
+    # """
+    # def __calculate(self, curr_val, op:Operand, rhs):
+    #     func_name = self.__operations[op.op.value]
+    #     func = getattr(self,func_name)
+    #     return func(curr_val, rhs)
+
+    # def __Sum(self, val1, val2):
+    #     return val1 + val2
+
+    # def __Dif(self, val1, val2):
+    #     return val1 - val2
+
+    # def __Mul(self, val1, val2):
+    #     return val1 * val2
+
+    # def __Div(self, val1, val2):
+    #     if val1 == 0 or val2 == 0
+    #         raise ZeroDivisionError("Cannot divide by 0!")
+    #     return val1 / val2
+
+    # def __Pow(self, val1, val2):
+    #     return val1 ** val2
