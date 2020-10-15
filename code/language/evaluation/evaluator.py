4535f0abfd83e86e706fd60c3c11ff9a3b35280e
sofiamc9 <sofiamc9@students.cs.ubc.ca>
2020-10-15 02:05:43 -0700

    implemented FastFunc evaluations

--- code/language/evaluation/evaluator.py
+++ code/language/evaluation/evaluator.py
@@ -4,25 +4,15 @@ if STATIC_CHECK:
     from code.language.shared.ast import *
 from code.language.shared.ast.visitor import Visitor
 from code.language.shared.primitives import values, numerical
-from code.targets.analysis.math import apply_fn, apply_op
+from code.targets.analysis.math import apply_fn, apply_op, apply_qk
 from .errors import LanguageError
 from .vars import Environment
 
 
 class Evaluator(Visitor):
-    # variable_table = {}
-    # data = {}
 
     def __init__(self):
         self.env = Environment()
-
-    # __operations = {
-    #     "+": "__Sum",
-    #     "-": "__Dif",
-    #     "*": "__Mul",
-    #     "/": "__Div",
-    #     "^": "__Pow"
-    # }
 
     def evaluate(self, p: Program):
         return self.visit_program(p)
@@ -46,12 +36,6 @@ class Evaluator(Visitor):
         pass
 
     def visit_trigger(self, t: Trigger):
-        # try:
-        #     # data_source = self.data[t.var]
-        #     # implement Observer Pattern to watch for data updates
-        # except:
-        #     raise KeyError("Cannot run mathematics on invalid data")
-        # else:
         t.math_funcs.accept(self)
 
     def visit_plotter(self, pltr: Plotter):
@@ -88,12 +72,6 @@ class Evaluator(Visitor):
         pass
 
     def visit_simple_func(self, smp: SimpleFunc) -> values.Value:
-
-        # if smp.impacted_var not in self.variable_table:
-        #     self.variable_table[smp.impacted_var.name] = 0
-        
-        # curr_val = self.variable_table[smp.impacted_var.name]
-        # num = 0
         if isinstance(smp.rhs.value, values.NumericalValue):
             v: values.Value = self.env.get_val(smp.impacted_var.name)
             if isinstance(v, values.NumericalValue):
@@ -104,14 +82,19 @@ class Evaluator(Visitor):
         else:
             raise LanguageError("Simple Functions only accept numbers.")
 
-        # self.variable_table[smp.impacted_var.name] = self.__calculate(curr_val, smp.op, num)
-        pass
+    def visit_increment(self, i: Increment) -> values.Value:
+        v: values.Value = self.env.get_val(i.impacted_var.name)
+        if isinstance(v, values.NumericalValue):
+            return apply_qk("inc",v)
+        else:
+            raise LanguageError("Fast Functions only accept numbers.")
 
-    def visit_increment(self, i: Increment):
-        pass
-
-    def visit_decrement(self, d: Decrement):
-        pass
+    def visit_decrement(self, d: Decrement) -> values.Value:
+        v: values.Value = self.env.get_val(d.impacted_var.name)
+        if isinstance(v, values.NumericalValue):
+            return apply_qk("dec",v)
+        else:
+            raise LanguageError("Fast Functions only accept numbers.")
 
     def visit_builtin_func(self, bltn: BuiltinFunc):
         v: values.Value = self.env.get_val(bltn.input.name)
@@ -122,30 +105,3 @@ class Evaluator(Visitor):
 
     def visit_operand(self, op: Operand) -> numerical.NumOp:
         return op.op
-
-    # """
-    # To call calculation methods dynamically using a dictionary.
-    # Per this post:
-    # https://stackoverflow.com/questions/16642145/how-to-dynamically-call-methods-within-a-class-using-method-name-assignment-to-a?lq=1
-    # """
-    # def __calculate(self, curr_val, op:Operand, rhs):
-    #     func_name = self.__operations[op.op.value]
-    #     func = getattr(self,func_name)
-    #     return func(curr_val, rhs)
-
-    # def __Sum(self, val1, val2):
-    #     return val1 + val2
-
-    # def __Dif(self, val1, val2):
-    #     return val1 - val2
-
-    # def __Mul(self, val1, val2):
-    #     return val1 * val2
-
-    # def __Div(self, val1, val2):
-    #     if val1 == 0 or val2 == 0
-    #         raise ZeroDivisionError("Cannot divide by 0!")
-    #     return val1 / val2
-
-    # def __Pow(self, val1, val2):
-    #     return val1 ** val2
