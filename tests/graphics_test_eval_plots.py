9ac978d1db7851fa0bef9cd9f7257391db42add6
rowdl22 <rowdl22@students.cs.ubc.ca>
2020-10-16 02:31:02 -0700

    add a test case

--- tests/graphics_test_eval_plots.py
+++ tests/graphics_test_eval_plots.py
@@ -3,6 +3,7 @@ from tests.util.example_ast import simple_plot_example
 from code.language.evaluation import Evaluator
 from code.language.evaluation.errors import LanguageError
 from code.language.evaluation.vars import UndefinedVariableError
+from code.language.shared.primitives.values import FloatValue
 
 
 class GraphicsEvaluationTests(TestCase):
@@ -22,3 +23,14 @@ class GraphicsEvaluationTests(TestCase):
         code, err = e.evaluate(program)
         self.assertNotEqual(0, code)
         self.assertTrue(isinstance(err, UndefinedVariableError))
+
+    def test_plot_manual_define(self):
+        """
+        Should successfully create a plot and update with
+        a constant value
+        """
+        program = simple_plot_example()
+        e = Evaluator(graphics=False)
+        e.env.extend('t', FloatValue(0.5))
+        code, err = e.evaluate(program)
+        self.assertEqual(0, code)
