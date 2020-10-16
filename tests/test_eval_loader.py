77519fea4081d173a971d00a1b577e46774a83aa
rowdl22 <rowdl22@students.cs.ubc.ca>
2020-10-16 16:14:40 -0700

    fix loader test with count variable

--- tests/test_eval_loader.py
+++ tests/test_eval_loader.py
@@ -3,6 +3,8 @@ from code.language.evaluation import Evaluator
 from code.language.shared import ast
 from code.language.shared.primitives.misc import ReportingMode
 from code.targets.data.dataLoader import DataLoaderError
+from code.language.shared.primitives import Types
+from code.language.shared.primitives.values import IntegerValue
 
 
 class LoaderEvaluationTests(TestCase):
@@ -55,6 +57,8 @@ class LoaderEvaluationTests(TestCase):
                 ast.Loader(ast.Var("source"),
                            ast.Source(ast.Reporting(ReportingMode.LIVE),
                                       "https://covid-api.com/api/reports")),
+                ast.Assigner(ast.Declare(ast.Type(Types.NUMBER), ast.Var("count")),
+                             ast.Value(IntegerValue(0))),
                 ast.Trigger(ast.Var("source"), ast.MathFuncs([ast.Increment(ast.Var(
                     "count"))])),
             ]))
