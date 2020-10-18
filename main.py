46cc07ebdc62041f27d62e50886db5df9c9294bc
rowdl22 <rowdl22@students.cs.ubc.ca>
2020-10-17 21:00:59 -0700

    create entry script

--- main.py
+++
@@ -1,7 +0,0 @@
-from mystix.targets.data import dataLoader
-from mystix.targets.visualization import graphs_example
-
-
-if __name__ == "__main__":
-    print("Doing Main Stuff")
-    graphs_example.run()
---
+++ mystix/__main__.py
@@ -0,0 +1,4 @@
+import sys
+
+if len(sys.argv) >= 2 and sys.argv[1] == "-v":
+    print("Mystix v0.0.9")
